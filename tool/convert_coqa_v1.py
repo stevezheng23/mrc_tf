ba74a871091830b9ff841a9f11b62f0898f9016c
Mingzhi Zheng <mizheng@microsoft.com>
2019-08-09 16:57:14 -0700

    add conversion & evaluation scripts for coqa task

---
+++ tool/convert_coqa_v1.py
@@ -0,0 +1,33 @@
+import argparse
+import json
+
+def add_arguments(parser):
+    parser.add_argument("--input_file", help="path to input file", required=True)
+    parser.add_argument("--output_file", help="path to output file", required=True)
+
+def convert_coqa(input_file,
+                 output_file):
+    with open(input_file, "r") as file:
+        input_data = json.load(file)
+    
+    output_data = []
+    for data in input_data:
+        id_items = data["qas_id"].split('_')
+        id = id_items[0]
+        turn_id = int(id_items[1])
+        answer = data["predict_text"]
+        
+        output_data.append({
+            "id": id,
+            "turn_id": turn_id,
+            "answer": answer
+        })
+    
+    with open(output_file, "w") as file:
+        json.dump(output_data, file, indent=4)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    add_arguments(parser)
+    args = parser.parse_args()
+    convert_coqa(args.input_file, args.output_file)
