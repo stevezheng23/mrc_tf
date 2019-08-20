017dc3cd5c265d82678072474a8bf9f3a9fc3ac3
Mingzhi Zheng <mizheng@microsoft.com>
2019-08-19 18:03:35 -0700

    update coqa convertion script to support unk/yes/no answer

--- tool/convert_coqa_v1.py
+++ tool/convert_coqa_v1.py
@@ -1,12 +1,16 @@
 import argparse
 import json
 
+import numpy as np
+
 def add_arguments(parser):
     parser.add_argument("--input_file", help="path to input file", required=True)
     parser.add_argument("--output_file", help="path to output file", required=True)
+    parser.add_argument("--prob_threshold", help="probability threshold", required=False, default=0.1, type=float)
 
 def convert_coqa(input_file,
-                 output_file):
+                 output_file,
+                 prob_threshold):
     with open(input_file, "r") as file:
         input_data = json.load(file)
     
@@ -15,7 +19,23 @@ def convert_coqa(input_file,
         id_items = data["qas_id"].split('_')
         id = id_items[0]
         turn_id = int(id_items[1])
+        
+        prob_list = [
+            data["unk_answer_prob"],
+            data["yes_answer_prob"],
+            data["no_answer_prob"]
+        ]
+        
+        prob_idx = np.argmax(prob_list)
+        
         answer = data["predict_text"]
+        if prob_list[prob_idx] > prob_threshold:
+            if prob_idx == 0:
+                answer = "unknown"
+            elif prob_idx == 1:
+                answer = "yes"
+            elif prob_idx == 2:
+                answer = "no"
         
         output_data.append({
             "id": id,
@@ -30,4 +50,4 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     add_arguments(parser)
     args = parser.parse_args()
-    convert_coqa(args.input_file, args.output_file)
+    convert_coqa(args.input_file, args.output_file, args.prob_threshold)
