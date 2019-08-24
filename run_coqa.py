fix activation & result masking issue in xlnet-coqa model

run_coqa.py
@@ -1126,7 +1126,7 @@ class XLNetModelBuilder(object):
                     end_result = tf.concat([output_result, feat_result], axis=-1)                          # [b,l,h], [b,l,h] --> [b,l,2h]
                     end_result_mask = 1 - p_mask                                                                                   # [b,l]
                     
-                    end_result = tf.layers.dense(end_result, units=self.model_config.d_model, activation=tf.relu,
+                    end_result = tf.layers.dense(end_result, units=self.model_config.d_model, activation=tf.nn.relu,
                         use_bias=True, kernel_initializer=initializer, bias_initializer=tf.zeros_initializer,
                         kernel_regularizer=None, bias_regularizer=None, trainable=True, name="end_modeling")        # [b,l,2h] --> [b,l,h]
                     
@@ -1153,7 +1153,7 @@ class XLNetModelBuilder(object):
                     end_result_mask = tf.expand_dims(1 - p_mask, axis=1)                                               # [b,l] --> [b,1,l]
                     end_result_mask = tf.tile(end_result_mask, multiples=[1,FLAGS.start_n_top,1])                    # [b,1,l] --> [b,k,l]
                     
-                    end_result = tf.layers.dense(end_result, units=self.model_config.d_model, activation=tf.relu,
+                    end_result = tf.layers.dense(end_result, units=self.model_config.d_model, activation=tf.nn.relu,
                         use_bias=True, kernel_initializer=initializer, bias_initializer=tf.zeros_initializer,
                         kernel_regularizer=None, bias_regularizer=None, trainable=True, name="end_modeling")    # [b,l,k,2h] --> [b,l,k,h]
                     
@@ -1181,7 +1181,7 @@ class XLNetModelBuilder(object):
                 answer_result = tf.squeeze(answer_result, axis=1)                                                    # [b,1,2h] --> [b,2h]
                 answer_result_mask = tf.reduce_max(1 - p_mask, axis=-1)                                                    # [b,l] --> [b]
                 
-                answer_result = tf.layers.dense(answer_result, units=self.model_config.d_model, activation=tf.relu,
+                answer_result = tf.layers.dense(answer_result, units=self.model_config.d_model, activation=tf.nn.relu,
                     use_bias=True, kernel_initializer=initializer, bias_initializer=tf.zeros_initializer,
                     kernel_regularizer=None, bias_regularizer=None, trainable=True, name="answer_modeling")             # [b,2h] --> [b,h]
                 
@@ -1194,7 +1194,7 @@ class XLNetModelBuilder(object):
                         kernel_regularizer=None, bias_regularizer=None, trainable=True, name="unk_answer_project")       # [b,h] --> [b,1]
                     
                     unk_answer_result = tf.squeeze(unk_answer_result, axis=-1)                                             # [b,1] --> [b]
-                    unk_answer_result = self._generate_masked_data(unk_answer_result, unk_answer_result_mask)           # [b], [b] --> [b]
+                    unk_answer_result = self._generate_masked_data(unk_answer_result, answer_result_mask)               # [b], [b] --> [b]
                     unk_answer_prob = tf.sigmoid(unk_answer_result)                                                                  # [b]
                     predicts["unk_answer_prob"] = unk_answer_prob
                 
@@ -1204,7 +1204,7 @@ class XLNetModelBuilder(object):
                         kernel_regularizer=None, bias_regularizer=None, trainable=True, name="yes_answer_project")       # [b,h] --> [b,1]
                     
                     yes_answer_result = tf.squeeze(yes_answer_result, axis=-1)                                             # [b,1] --> [b]
-                    yes_answer_result = self._generate_masked_data(yes_answer_result, yes_answer_result_mask)           # [b], [b] --> [b]
+                    yes_answer_result = self._generate_masked_data(yes_answer_result, answer_result_mask)               # [b], [b] --> [b]
                     yes_answer_prob = tf.sigmoid(yes_answer_result)                                                                  # [b]
                     predicts["yes_answer_prob"] = unk_answer_prob
                 
@@ -1214,7 +1214,7 @@ class XLNetModelBuilder(object):
                         kernel_regularizer=None, bias_regularizer=None, trainable=True, name="no_answer_project")        # [b,h] --> [b,1]
                     
                     no_answer_result = tf.squeeze(no_answer_result, axis=-1)                                               # [b,1] --> [b]
-                    no_answer_result = self._generate_masked_data(no_answer_result, no_answer_result_mask)              # [b], [b] --> [b]
+                    no_answer_result = self._generate_masked_data(no_answer_result, answer_result_mask)                 # [b], [b] --> [b]
                     no_answer_prob = tf.sigmoid(no_answer_result)                                                                    # [b]
                     predicts["no_answer_prob"] = no_answer_prob
             