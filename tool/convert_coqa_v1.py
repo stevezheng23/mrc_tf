c5be69f3fcd2a6cec0b8f6aebff2f7ea3ac3c75c
Mingzhi Zheng <mizheng@microsoft.com>
2019-08-25 17:31:38 -0700

    update coqa conversion tool to adopt latest output format

--- tool/convert_coqa_v1.py
+++ tool/convert_coqa_v1.py
@@ -6,11 +6,9 @@ import numpy as np
 def add_arguments(parser):
     parser.add_argument("--input_file", help="path to input file", required=True)
     parser.add_argument("--output_file", help="path to output file", required=True)
-    parser.add_argument("--prob_threshold", help="probability threshold", required=False, default=0.1, type=float)
 
 def convert_coqa(input_file,
-                 output_file,
-                 prob_threshold):
+                 output_file):
     with open(input_file, "r") as file:
         input_data = json.load(file)
     
@@ -20,27 +18,17 @@ def convert_coqa(input_file,
         id = id_items[0]
         turn_id = int(id_items[1])
         
-        prob_list = [
-            data["unk_answer_prob"],
-            data["yes_answer_prob"],
-            data["no_answer_prob"]
-        ]
+        predict_id = data["answer_predict_id"]
+        predict_score = data["answer_predict_score"]
         
-        prob_idx = np.argmax(prob_list)
-        
-        answer = data["predict_text"]
-        if prob_list[prob_idx] > prob_threshold:
-            if prob_idx == 0:
-                answer = "unknown"
-            elif prob_idx == 1:
-                answer = "yes"
-            elif prob_idx == 2:
-                answer = "no"
+        predict_text_list = [data["predict_text"], "unknown", "yes", "no"]
+        predict_text = predict_text_list[predict_id]
         
         output_data.append({
             "id": id,
             "turn_id": turn_id,
-            "answer": answer
+            "answer": predict_text,
+            "score": predict_score
         })
     
     with open(output_file, "w") as file:
@@ -50,4 +38,4 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     add_arguments(parser)
     args = parser.parse_args()
-    convert_coqa(args.input_file, args.output_file, args.prob_threshold)
+    convert_coqa(args.input_file, args.output_file)
