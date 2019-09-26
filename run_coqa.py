11308db536f52f7f495effaad3a7ce5dffe7586e
Mingzhi Zheng <mizheng@microsoft.com>
2019-09-25 19:25:06 -0700

    disable span matching onnly for unk/yes/no answer in xlnet-coqa runner

--- run_coqa.py
+++ run_coqa.py
@@ -370,9 +370,6 @@ class CoqaPipeline(object):
                          answer,
                          answer_type,
                          paragraph_text):
-        if answer_type != "span":
-            return "", -1, -1, False
-        
         input_text = answer["input_text"].strip().lower()
         span_start, span_end = answer["span_start"], answer["span_end"]
         if span_start == -1 or span_end == -1:
@@ -387,7 +384,7 @@ class CoqaPipeline(object):
         
         if span_start == -1 or span_end == -1:
             answer_text = ""
-            is_skipped = True
+            is_skipped = (answer_type == "span")
         else:
             answer_text = paragraph_text[span_start:span_end+1]
             is_skipped = False
@@ -401,16 +398,11 @@ class CoqaPipeline(object):
         if norm_answer in ["yes", "yese", "ye", "es"]:
             return "yes"
         
-        if norm_answer in ["no", "no not at all", "not", "not yet", "not really"]:
+        if norm_answer in ["no", "no not at all", "not", "not at all"]:
             return "no"
         
-        number_lookup = {
-            "zero": "none", "0": "none", "1": "one", "2": "two", "3": "three", "4": "four",
-            "5": "five", "6": "six", "7": "seven", "8": "eight", "9": "nine", "10": "ten"
-        }
-        
-        if norm_answer in number_lookup:
-            return number_lookup[norm_answer]
+        if norm_answer in ["0", "zero"]
+            return "none"
         
         return norm_answer
     
@@ -804,7 +796,7 @@ class XLNetExampleProcessor(object):
             token2char_raw_start_index.append(raw_start_pos)
             token2char_raw_end_index.append(raw_end_pos)
         
-        if example.answer_type == "span" and not example.is_skipped and example.orig_answer_text:
+        if example.answer_type not in ["unknown", "yes", "no"] and not example.is_skipped and example.orig_answer_text:
             raw_start_char_pos = example.start_position
             raw_end_char_pos = raw_start_char_pos + len(example.orig_answer_text) - 1
             tokenized_start_char_pos = self._convert_tokenized_index(raw2tokenized_char_index, raw_start_char_pos, is_start=True)
@@ -918,7 +910,7 @@ class XLNetExampleProcessor(object):
             else:
                 option = 0
             
-            if example.answer_type == "span" and not example.is_skipped and example.orig_answer_text:
+            if example.answer_type not in ["unknown", "yes", "no"] and not example.is_skipped and example.orig_answer_text:
                 doc_start = doc_span["start"]
                 doc_end = doc_start + doc_span["length"] - 1
                 if tokenized_start_token_pos >= doc_start and tokenized_end_token_pos <= doc_end:
@@ -947,7 +939,7 @@ class XLNetExampleProcessor(object):
                 printable_input_tokens = [prepro_utils.printable_text(input_token) for input_token in input_tokens]
                 tf.logging.info("input_tokens: %s" % input_tokens)
                 
-                if example.answer_type == "span" and not example.is_skipped and example.orig_answer_text:
+                if example.answer_type not in ["unknown", "yes", "no"] and not example.is_skipped and example.orig_answer_text:
                     tf.logging.info("start_position: %s" % str(start_position))
                     tf.logging.info("end_position: %s" % str(end_position))
                     answer_tokens = input_tokens[start_position:end_position+1]
