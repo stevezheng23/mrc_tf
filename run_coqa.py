d5f8d15878e9709fd75bc4e9ce3c3465368e32b6
Mingzhi Zheng <mizheng@microsoft.com>
2019-11-16 16:31:01 -0800

    make non-span processing consistent

--- run_coqa.py
+++ run_coqa.py
@@ -469,7 +469,7 @@ class CoqaPipeline(object):
                 question_text = self._get_question_text(question_history, question)
                 question_history = self._get_question_history(question_history, question, answer, answer_type, is_skipped, self.num_turn)
                 
-                if answer_type != "unknown" and not is_skipped:
+                if answer_type not in ["unknown", "yes", "no"] and not is_skipped and answer_text:
                     start_position = span_start
                     orig_answer_text = self._process_found_answer(answer["input_text"], answer_text)
                 else:
